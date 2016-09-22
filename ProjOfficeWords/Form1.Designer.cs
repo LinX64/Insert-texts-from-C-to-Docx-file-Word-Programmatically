0f28830f59eeca5867ede9742447063fc6cd0e28
Mohsen LinX64 <ash.wxrz@hotmail.com>
2016-09-22 11:44:46 +0330

    Add and fixed some mistakes

---
+++ ProjOfficeWords/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ProjOfficeWords
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.lblSubject.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblSubject.Location = new System.Drawing.Point(460, 77);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(79, 13);
+            this.lblSubject.TabIndex = 0;
+            this.lblSubject.Text = "Speech Title ";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(66, 74);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtSubject.Size = new System.Drawing.Size(291, 20);
+            this.txtSubject.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(66, 120);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtTitle.Size = new System.Drawing.Size(291, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblTitle.Location = new System.Drawing.Point(435, 120);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(104, 13);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Speecher\'s Name";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(66, 163);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtAddress.Size = new System.Drawing.Size(291, 20);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.lblAddress.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblAddress.Location = new System.Drawing.Point(507, 163);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(32, 13);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Title";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(260, 220);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Output";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(387, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "The subject of the speech";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(66, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTimePicker1.Size = new System.Drawing.Size(291, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 365);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.lblSubject);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "Preparing speech";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ ProjOfficeWords/Form1.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+
+
+namespace ProjOfficeWords
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            //specify the path of ms word docx
+            string fileName = @"C:\Users\LinX64\Desktop\ProjOfficeWords\ProjOfficeWords\sample1.docx";
+            
+
+            try
+            {
+
+                // Create a word Application
+                Microsoft.Office.Interop.Word.Application myapp1 = new Microsoft.Office.Interop.Word.Application();
+                Document mydoc1;
+                //define the word docx
+                mydoc1 = myapp1.Documents.Open(fileName);
+
+                // Activate the word docx
+                mydoc1.Activate();
+                myapp1.Visible = true; // Let's show the output!
+
+
+                //get all Bookmarks
+                Bookmarks bk = mydoc1.Bookmarks;
+                bk["dtSubject"].Range.Text = dateTimePicker1.Value.Date.ToString();
+                bk["Subject"].Range.Text = txtSubject.Text;
+                bk["FullName"].Range.Text = txtTitle.Text;
+                bk["Address"].Range.Text = txtAddress.Text;
+
+                MessageBox.Show("Successfull!");
+
+            }
+
+           
+            catch (Exception ex)
+            {
+                MessageBox.Show("UnSuccessfull!");
+            }
+
+
+
+ }
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
